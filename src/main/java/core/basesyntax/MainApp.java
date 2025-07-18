01e5b3b8e085a2f86b1437f44ea9b518eee5ae96
AliakseiS97 <aliakseis97@gmail.com>
2025-07-18 22:16:28 +0200

    The first solution for the  task "OOP Basic"

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,22 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Truck truck = new Truck();
+        truck.doWork();
+        truck.stopWork();
 
+        Bulldozer bulldozer = new Bulldozer();
+        bulldozer.doWork();
+        bulldozer.stopWork();
+
+        Excavator excavator = new Excavator();
+        excavator.doWork();
+        excavator.stopWork();
+
+        for (String a : args) {
+            System.out.println(a);
+        }
+
+    }
 }
